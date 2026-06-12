01e9be50532f5767c16c0324c925603a3c49bf3d
oldlay <2391939406@qq.com>
2026-06-12 12:14:34 +0800

    grpc parameters validation and db migration in main.go

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"net"
 	"net/http"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"github.com/oldlay/simplebank/api"
@@ -33,11 +36,27 @@ func main() {
 		log.Fatal("cannot connect to db:", err)
 	}
 
+	runDBMigration(config.MigrationURL, config.DBSource)
+
 	store := db.NewStore(conn)
 
 	go runGatewayServer(config, store)
 	runGrpcServer(config, store)
 	//runGinServer(config, store)
+}
+
+func runDBMigration(migrationURL string, dbSource string) {
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal("cannot create migration instance:", err)
+	}
+
+	if err = migration.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal("failed to run migrate up:", err)
+	}
+
+	log.Println("db migrated successfully")
+
 }
 
 // runGinServer starts the HTTP server with Gin framework
@@ -102,7 +121,7 @@ func runGatewayServer(config util.Config, store db.Store) {
 
 	statikFS, err := fs.New()
 	if err != nil {
-		log.Fatal("cannot create statik file system: %s", err)
+		log.Fatalf("cannot create statik file system: %s", err)
 	}
 	swaggerHander := http.StripPrefix("/swagger/", http.FileServer(statikFS))
 	mux.Handle("/swagger/", swaggerHander)
--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 # script exit when commends return with a non-zero status
 set -e
 
-echo "run db migration"
-/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+#echo "run db migration"
+#/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start it up"
 exec "$@"
